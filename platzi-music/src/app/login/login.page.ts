196c41cf90bce9e07520fbf11b937a85c8bbc4b4
Juan Sáez García <contacto@juamber.com>
2022-03-08 20:59:33 +0100

    Crear una página de login

---
+++ platzi-music/src/app/login/login.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  public srcLogo: string = "assets/img/logo.png";
+  public loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"),
+      ]))
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  loginUser(algo: any){
+
+  }
+
+}
