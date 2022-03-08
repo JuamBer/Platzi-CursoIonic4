b64208f3143fa2d3229c9a41b6fb03fc369075f7
Juan Sáez García <contacto@juamber.com>
2022-03-08 21:21:32 +0100

    Validar inputs del formulario de login

--- platzi-music/src/app/login/login.page.ts
+++ platzi-music/src/app/login/login.page.ts
@@ -10,6 +10,28 @@ export class LoginPage implements OnInit {
 
   public srcLogo: string = "assets/img/logo.png";
   public loginForm: FormGroup;
+  public validationMessages = {
+    email: [
+      {
+        type: "required",
+        message: "El email es requerido"
+      },
+      {
+        type: "pattern",
+        message: "El email es incorrecto"
+      }
+    ],
+    password: [
+      {
+        type: "required",
+        message: "El password es requerido"
+      },
+      {
+        type: "minlength",
+        message: "La longitud mínima para la contraseña es de 5 caracteres"
+      }
+    ]
+  }
 
   constructor(
     private formBuilder: FormBuilder
@@ -18,6 +40,10 @@ export class LoginPage implements OnInit {
       email: new FormControl("", Validators.compose([
         Validators.required,
         Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"),
+      ])),
+      password: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
       ]))
     });
   }
