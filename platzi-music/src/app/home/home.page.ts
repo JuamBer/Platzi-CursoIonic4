4f19fdbfcc5b402340da3d446be65a830e292bb6
Juan Sáez García <contacto@juamber.com>
2022-03-19 21:48:49 +0100

    api

--- platzi-music/src/app/home/home.page.ts
+++ platzi-music/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Artist } from 'src/modules/Artist';
+import { APIService } from '../services/api.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,13 +13,27 @@ export class HomePage {
     slidesPerView: 4,
     speed:400
   }
-  public artists: Artist[] = [
-    new Artist("Juan", "https://via.placeholder.com/150"),
-    new Artist("Juan", "https://via.placeholder.com/150"),
-    new Artist("Juan", "https://via.placeholder.com/150"),
-    new Artist("Juan", "https://via.placeholder.com/150")
-  ];
+  public artists: any[] = [];
+  public songs: any[] = [];
+  public albums: any[] = [];
 
-  constructor() {}
+  constructor(
+    private musicService: APIService
+  ) {}
+
+  ionViewDidEnter() {
+    this.musicService.getNewReleases().then(
+      (newRealeses)=>{
+        this.songs = newRealeses.albums.items.filter(e=>e.album_type=="single");
+        this.albums = newRealeses.albums.items.filter(e => e.album_type == "album");
+      }
+    )
+
+    this.musicService.getArtist().then(
+      (artists)=>{
+        this.artists = artists.items
+      }
+    )
+  }
 
 }
---
+++ platzi-music/src/app/services/api.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode, HttpHeaders } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class APIService {
+
+  private apiUrl: string = environment.API_URL;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getArtist() {
+    return fetch(`https://static.platzi.com/media/tmp/class-files/git/ionic/ionic-0b43f4f65e1026d2afe15307334d28bda0307cc3/src/app/services/artists.json`).then(
+      response => response.json()
+    );
+  }
+
+  getNewReleases() {
+    return fetch(`https://platzi-music-api.herokuapp.com/browse/new-releases`).then(
+      response => response.json()
+    );
+  }
+}
